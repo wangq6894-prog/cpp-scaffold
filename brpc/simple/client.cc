8c695f983bdae0388c8a8790a4e89ad84ac485e0
wangq6894-prog <wangq6894@gmail.com>
2026-08-31 14:57:49 +0000

    brpc-simple

---
+++ brpc/simple/client.cc
@@ -0,0 +1,26 @@
+#include <brpc/channel.h>
+#include "cal.pb.h"
+#include <iostream>
+
+int main(int argc,char* argv[]){
+    //0.实例化ChannelOptions对象
+    brpc::ChannelOptions options;
+    options.protocol = "baidu_std";
+    //1.实例化Channel对象
+    brpc::Channel channel;
+    channel.Init("192.168.8.128:9000",&options);
+    //2.实例化CalService_stub对象--用于发起rpc请求
+    cal::CalService_Stub stub(&channel);
+    brpc::Controller cntl;
+    cal::AddReq request;
+    request.set_num1(10);
+    request.set_num2(20);
+    cal::AddRsp response;
+    stub.Add(&cntl,&request,&response,NULL);
+    if(cntl.Failed() == true){
+        std::cout << "rpc请求失败：" << cntl.ErrorText() << std::endl;
+        return -1;
+    }
+    std::cout << "Add result:" << response.result() << std::endl;
+    return 0;
+}
